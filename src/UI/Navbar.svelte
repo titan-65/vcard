56cca5cfe77561a0cee397256e0cc84c2856018c
Vantol Bennett <vvantol2000@icloud.com>
2020-11-11 07:21:12 -0500

    Additional files created

---
+++ src/UI/Navbar.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let label;
+</script>
+<div>
+    <nav
+            class="navbar navbar-inverse bg-dark fixed-top navbar-expand-lg"
+            id="sectionsNav">
+        <div class="container">
+            <div class="navbar-translate">
+                    <a class="navbar-brand"> {label} </a>
+                <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon" />
+                    <span class="navbar-toggler-icon" />
+                    <span class="navbar-toggler-icon" />
+                </button>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="dropdown nav-item">
+                        <!-- svelte-ignore a11y-missing-attribute -->
+                        <a class="nav-link">
+                                <i class="material-icons">view_carousel</i>
+                                Player Card
+
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
---
+++ src/UI/VCard.svelte
@@ -0,0 +1,14 @@
+<script></script>
+<div class="card vcard">
+    <div class="card-body">
+        <img class="card-avatar" src="" alt="">
+        <div class="card-description">
+
+        </div>
+    </div>
+</div>
+<style>
+    .vcard {
+        height: 400px;
+    }
+</style>
